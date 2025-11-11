97bca4bba9465d9c5d29ce584ba472726724bdd8
1perfect-cell <saitama1345@gmail.com>
2025-11-11 21:41:55 +0700

    ubah warna header

--- header.php
+++ header.php
@@ -6,17 +6,20 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Alat Kesehatan Rumah Sakit Fulan</title>
+  <title>Calibration Management</title>
   <!-- Bootstrap 5 CDN -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <!-- Custom CSS -->
   <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
+<nav class="navbar navbar-expand-lg navbar-light custom-yellow mb-4">
   <div class="container">
-    <a class="navbar-brand" href="index.php">ALKES</a>
-    <div class="collapse navbar-collapse">
+    <a class="navbar-brand" href="index.php">Calibration</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-auto">
         <li class="nav-item"><a class="nav-link" href="index.php">Daftar</a></li>
         <li class="nav-item"><a class="nav-link" href="create.php">Tambah</a></li>
